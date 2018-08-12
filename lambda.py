9b2d2b01e0d8c478f8310c79fdbf04d38c2fc380
marekq <marek.kuczynski@gmail.com>
2018-08-12 22:20:24 +0200

    added multi threading, new feeds, bugfixes

--- lambda.py
+++ lambda.py
@@ -2,33 +2,47 @@
 # @marekq
 # www.marek.rocks
 
-import boto3, feedparser, re, os, time
+import boto3, feedparser, re, os, Queue, threading, time
 from botocore.vendored import requests
 from boto3.dynamodb.conditions import Key, Attr
 from bs4 import *
 
+# establish a session with DynamoDB
+def dynamo_sess():
+	return boto3.resource('dynamodb', region_name = os.environ['dynamo_region']).Table(os.environ['dynamo_table'])
+
+s		= dynamo_sess()
+q1     	= Queue.Queue()
+
 feeds	= {
-	'whats-new' 			: 'https://aws.amazon.com/new/feed/', 
-	'newsblog'				: 'http://feeds.feedburner.com/AmazonWebServicesBlog',
-	"devops"				: "http://blogs.aws.amazon.com/application-management/blog/feed/recentPosts.rss",
-	"big-data"				: "http://blogs.aws.amazon.com/bigdata/blog/feed/recentPosts.rss",
-	"security"				: "http://blogs.aws.amazon.com/security/blog/feed/recentPosts.rss",
-	"java"					: "http://java.awsblog.com/blog/feed/recentPosts.rss",
-	"mobile"				: "http://mobile.awsblog.com/blog/feed/recentPosts.rss",
-	"architecture"			: "http://www.awsarchitectureblog.com/atom.xml",
+	"public-sector"			: "https://aws.amazon.com/blogs/publicsector/feed/",
+	"gamedev"				: "https://aws.amazon.com/blogs/gamedev/feed/",
+	"ml"					: "https://aws.amazon.com/blogs/machine-learning/feed/",
+	"cli"					: "https://aws.amazon.com/blogs/developer/category/programing-language/aws-cli/feed/",
+	"whats-new" 			: "https://aws.amazon.com/new/feed/", 
+	"newsblog"				: "https://aws.amazon.com/blogs/aws/feed/",
+	"serverless" 			: "https://aws.amazon.com/blogs/aws/category/serverless/feed/",
+	"devops"				: "https://aws.amazon.com/application-management/blog/feed/",
+	"big-data"				: "https://aws.amazon.com/blogs/big-data/feed/",
+	"security"				: "https://aws.amazon.com/blogs/security/feed/",
+	"java"					: "https://aws.amazon.com/blogs/developer/category/programing-language/java/feed/",
+	"mobile"				: "https://aws.amazon.com/blogs/mobile/feed/",
+	"architecture"			: "https://aws.amazon.com/blogs/architecture/feed/",
 	"compute"				: "https://aws.amazon.com/blogs/compute/feed/",
 	"database"				: "https://aws.amazon.com/blogs/database/feed/",
 	"management-tools"		: "https://aws.amazon.com/blogs/mt/feed/",
 	"security-bulletins"	: "https://aws.amazon.com/security/security-bulletins/feed/"
 }
 
+# worker for queue jobs
+def worker():
+    while not q1.empty():
+        get_feed(q1.get())
+        q1.task_done()
+
 # get the RSS feed through feedparser
 def get_rss(url):
 	return feedparser.parse(url)
-
-# establish a session with DynamoDB
-def dynamo_sess():
-	return boto3.resource('dynamodb', region_name = os.environ['dynamo_region']).Table(os.environ['dynamo_table'])
 
 # get the timestamp of the latest blogpost stored in DynamoDB
 def ts_dynamo(s, source):
@@ -109,9 +123,11 @@ def comprehend(txt, title):
 	return(tags)
 
 # main function to kick off collection of an rss feed
-def get_feed(url, source, s):
-	d		= get_rss(url)
+def get_feed(f):
+	url 	= f[0]
+	source	= f[1]
 	
+	d		= get_rss(url)
 	maxts	= ts_dynamo(s, source)
 	t 		= s.get_item(Key = {'timest': maxts, 'source': source})
 
@@ -126,7 +142,7 @@ def get_feed(url, source, s):
 		des			= x['description'].encode('utf-8')
 		r 			= re.compile(r'<[^>]+>')
 		desc 		= r.sub('', des).strip('&nbsp;')
-		auth		= x['author']
+		auth		= x['author'].encode('utf-8')
 
 		if int(timest) > int(maxts):
 			print('retrieving '+link)
@@ -136,17 +152,17 @@ def get_feed(url, source, s):
 			put_dynamo(s, timest, title, desc, link, source, auth, tags)
 			
 			msg		= str('<html><body><h2>'+title+'</h2><br>'+desc+'<br><br><a href="'+link+'">view post here</a></body></html>')
-			send_mail(msg, title)
+			send_mail(msg, source.upper()+' - '+title)
 			
 			print('sending message for article '+title)
-		
-		else:
-			pass
-			#print('skip sending article '+title)
-			
-def lambda_handler(event, context): 
-	s		= dynamo_sess()
 
+def lambda_handler(event, context): 
 	for source, url in feeds.iteritems():
-		get_feed(url, source, s)
-		print('getting '+source)
+		q1.put([url, source])
+		#get_feed([url, source])
+
+	for x in range(5):
+		t = threading.Thread(target = worker)
+		t.daemon = True
+		t.start()
+	q1.join()
